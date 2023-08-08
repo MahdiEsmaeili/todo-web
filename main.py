a28af6e27505ab65e66ce9632f4160b77af65077
mahdi <mahdied2021@gamil.com>
2023-08-09 00:18:50 +0300

    implement completing a todo

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ st.write("Use this app to increase your productivity")
 
 todos = functions.get_todos()
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(placeholder="Enter a todo...", label="",
               on_change=add_todo, key="new_todo")
