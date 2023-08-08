5052d1aef7287afc125f374793ef7dbca2bece9b
mahdi <mahdied2021@gamil.com>
2023-08-09 00:07:10 +0300

    implement adding todos

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import streamlit as st
 
 import functions
 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("easy to use and user-friendly")
 st.write("Use this app to increase your productivity")
@@ -12,4 +19,5 @@ todos = functions.get_todos()
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(placeholder="Enter a todo...", label="")
+st.text_input(placeholder="Enter a todo...", label="",
+              on_change=add_todo, key="new_todo")
